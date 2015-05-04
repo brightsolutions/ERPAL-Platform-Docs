c78b47a978e8e67c4184b67d73b866c9f2973acc
Dirk Peter <dirk.peter@me.com>
2015-05-04 12:06:25 +0200

    new theme update

--- erpaltheme/js/theme.js
+++ erpaltheme/js/theme.js
@@ -1,58 +1,27 @@
-$( document ).ready(function() {
-    // Shift nav in mobile when clicking the menu.
-    $(document).on('click', "[data-toggle='wy-nav-top']", function() {
-      $("[data-toggle='wy-nav-shift']").toggleClass("shift");
-      $("[data-toggle='rst-versions']").toggleClass("shift");
-    });
-    // Close menu when you click a link.
-    $(document).on('click', ".wy-menu-vertical .current ul li a", function() {
-      $("[data-toggle='wy-nav-shift']").removeClass("shift");
-      $("[data-toggle='rst-versions']").toggleClass("shift");
-    });
-    $(document).on('click', "[data-toggle='rst-current-version']", function() {
-      $("[data-toggle='rst-versions']").toggleClass("shift-up");
-    });
-    // Make tables responsive
-    $("table.docutils:not(.field-list)").wrap("<div class='wy-table-responsive'></div>");
+$(document).ready(function () {
+  // syntax highlighting
+  hljs.initHighlightingOnLoad();
 
-    hljs.initHighlightingOnLoad();
-
-    $('table').addClass('docutils');
+  $('table').addClass('docutils');
 
   // material
   $('.dropdown-button').dropdown();
   $('.button-collapse').sideNav();
   $('h2, h3, h4, .scrollspy').scrollSpy();
-  $('.toc-wrapper').width($('.toc').width())
-    .pushpin({ top:0 });
-});
+  $('.toc-wrapper').pushpin({top: 0});
 
-window.SphinxRtdTheme = (function (jquery) {
-    var stickyNav = (function () {
-        var navBar,
-            win,
-            stickyNavCssClass = 'stickynav',
-            applyStickNav = function () {
-                if (navBar.height() <= win.height()) {
-                    navBar.addClass(stickyNavCssClass);
-                } else {
-                    navBar.removeClass(stickyNavCssClass);
-                }
-            },
-            enable = function () {
-                applyStickNav();
-                win.on('resize', applyStickNav);
-            },
-            init = function () {
-                navBar = jquery('nav.wy-nav-side:first');
-                win    = jquery(window);
-            };
-        jquery(init);
-        return {
-            enable : enable
-        };
-    }());
-    return {
-        StickyNav : stickyNav
-    };
-}($));
+  var tocTimer = undefined;
+
+  function setTocWidth () {
+    clearTimeout(tocTimer);
+    tocTimer = setTimeout(
+      function () {
+        $('.toc-wrapper').width($('.toc').width());
+      },
+      50
+    );
+  }
+
+  $(window).on('resize', setTocWidth)
+    .trigger('resize');
+});
